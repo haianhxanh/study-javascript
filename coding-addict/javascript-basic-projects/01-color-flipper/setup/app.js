2f53ac3acb636a25d4277eb0e9d98559a50abf63
Hai Anh Nguyenová <hanka@hais-air.home>
2022-08-28 12:41:25 +0200

    reviews project

--- coding-addict/javascript-basic-projects/01-color-flipper/setup/app.js
+++ coding-addict/javascript-basic-projects/01-color-flipper/setup/app.js
@@ -1,4 +1,21 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+// const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+// const btn = document.getElementById("btn");
+// const color = document.querySelector(".color");
+
+// btn.addEventListener("click", function () {
+//   // get random number between 0 - 3 - this is because we want to change the value of colors[0], colors[1], colors[2], colors[3]
+
+//   const randomNumber = getRandomNumber();
+//   document.body.style.backgroundColor = colors[randomNumber];
+//   color.textContent = colors[randomNumber];
+// });
+
+// function getRandomNumber() {
+//   return Math.floor(Math.random() * colors.length);
+// }
+
+// my solution: random rgba colors without limitation
 
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
@@ -6,11 +23,14 @@ const color = document.querySelector(".color");
 btn.addEventListener("click", function () {
   // get random number between 0 - 3 - this is because we want to change the value of colors[0], colors[1], colors[2], colors[3]
 
-  const randomNumber = getRandomNumber();
-  document.body.style.backgroundColor = colors[randomNumber];
-  color.textContent = colors[randomNumber];
+  const firstValue = getRandomNumber();
+  const secondValue = getRandomNumber();
+  const thirdValue = getRandomNumber();
+
+  document.body.style.backgroundColor = `rgb(${firstValue}, ${secondValue}, ${thirdValue})`;
+  color.textContent = `rgb(${firstValue}, ${secondValue}, ${thirdValue})`;
 });
 
-function getRandomNumber() {
-  return Math.floor(Math.random() * colors.length);
-}
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * 255);
+};
