008edbac2565b3f1d7454cb9e36d8c32417af4cd
Hai Anh Nguyenová <hanka@hais-air.home>
2022-08-28 14:24:33 +0200

    modal project

--- coding-addict/javascript-basic-projects/6-modal/setup/app.js
+++ coding-addict/javascript-basic-projects/6-modal/setup/app.js
@@ -2,3 +2,17 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+const modalBtn = document.querySelector(".modal-btn");
+const closeBtn = document.querySelector(".close-btn");
+const modalOverlay = document.querySelector(".modal-overlay");
+
+const toggleModalOverlay = () => {
+  modalOverlay.classList.toggle("open-modal");
+};
+modalBtn.addEventListener("click", () => {
+  toggleModalOverlay();
+});
+closeBtn.addEventListener("click", () => {
+  toggleModalOverlay();
+});
