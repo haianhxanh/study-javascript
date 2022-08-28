38b2e400360515cd59fc505bb406c475583a63f7
Hai Anh Nguyenová <hanka@hais-air.home>
2022-08-28 14:51:50 +0200

    questions

--- coding-addict/javascript-basic-projects/7-questions/setup/app.js
+++ coding-addict/javascript-basic-projects/7-questions/setup/app.js
@@ -1,2 +1,27 @@
 //using selectors inside the element
 // traversing the dom
+
+// 1st option - traversing the dom
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach((btn) => {
+//   btn.addEventListener("click", (e) => {
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle("show-text");
+//   });
+// });
+
+// 2nd option
+// select question and then the button within that question
+const questions = document.querySelectorAll(".question");
+questions.forEach((question) => {
+  const btn = question.querySelector(".question-btn");
+  btn.addEventListener("click", () => {
+    questions.forEach((item) => {
+      if (question != item) {
+        item.classList.remove("show-text");
+      }
+    });
+    question.classList.toggle("show-text");
+  });
+});
