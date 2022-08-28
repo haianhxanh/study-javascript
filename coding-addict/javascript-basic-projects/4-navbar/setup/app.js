a792498fa0a9c627394b98b3b684167f1c728217
Hai Anh Nguyenová <hanka@hais-air.home>
2022-08-28 13:04:55 +0200

    navbar project

--- coding-addict/javascript-basic-projects/4-navbar/setup/app.js
+++ coding-addict/javascript-basic-projects/4-navbar/setup/app.js
@@ -3,3 +3,21 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+const toggleNav = () => {
+  links.classList.toggle("show-links");
+};
+navToggle.addEventListener("click", () => {
+  toggleNav();
+});
+
+navToggle.addEventListener("mouseover", () => {
+  toggleNav();
+});
+
+navToggle.addEventListener("mouseout", () => {
+  toggleNav();
+});
